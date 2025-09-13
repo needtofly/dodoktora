cd37d699cc0278ea640a8b01bd3fcd906428aebd
Jan Sowa <jansowa007@gmail.com>
2025-09-13 12:54:15 +0200

    Payments: switch to PayU REST (create order + webhook + return); remove P24

--- app/platnosc/p24/return/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
-
-import P24ReturnClient from './Client';
-
-export default function P24ReturnPage() {
-  return <P24ReturnClient />;
-}
--- pages/api/platnosc/p24/mock.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Location', '/platnosc/p24/mock');
-  return res.status(302).end();
-}
